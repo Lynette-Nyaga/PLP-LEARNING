5b8145a38d039f55421ce33ae8962bd7767ae15f
Lynette-Nyaga <Lynethewriter00@gmail.com>
2024-02-27 13:49:46 +0300

    "week2 changes"

---
+++ PYTHON/Week2.py
@@ -0,0 +1,7 @@
+Num1 = int(input("Enter your favorite number: "))
+Num2 = int(input("Enter another number: "))
+Num3 = int(input("Enter your third favorite number:"))
+Num4 = int(input("Enter your fourth favorite number: "))
+print(Num1,Num2,Num3,Num4)
+
+print(Num1+Num2+Num3+Num4)
